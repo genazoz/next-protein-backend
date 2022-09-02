475f3765a61cc7d220060a792d972b0e202fd0f1
genazoz <40898799+genazoz@users.noreply.github.com>
2022-09-02 19:29:58 +0300

    Добавил данные для heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,20 @@ import { OrderEntity } from './order/entities/order.entity';
 import { OrderModule } from './order/order.module';
 import { OrderProductModule } from './order-product/order-product.module';
 import { OrderProductEntity } from './order-product/entities/order-product.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      host: 'ec2-34-247-172-149.eu-west-1.compute.amazonaws.com',
       type: 'postgres',
-      host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: 'kehxbrKEHXBR1',
-      database: 'protein',
+      username: 'yxfgenuvrumenx',
+      password:
+        '82ed6d726f365448b0a0275db3cfa81ebf60118cc0e5a3e57fde2050ab7fc3ce',
+      database: 'dh7sd1tivpqfq',
       entities: [UserEntity, ProductEntity, OrderEntity, OrderProductEntity],
       synchronize: true,
     }),
